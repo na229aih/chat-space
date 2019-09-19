9685c5cddaf6f8f4f7fedbe5aabfa57fabdc278c
na229aih <naoya.aihara.1127@gmail.com>
2019-09-19 19:29:07 +0900

    add message appnd

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,27 @@
 $(function(){
 
+  function build_message(message){
+    let html = `<div class="right-contents__center__top">
+                  <div class="right-contents__center__top__box">
+                    <div class="right-contents__center__top__box__user-name">
+                      ${message.name}
+                    </div>
+                    <div class="right-contents__center__top__box__time">
+                      ${message.date}
+                    </div>
+                  </div>
+                </div>`
+                // <div class="right-contents__center__bottom">
+                //   ${if(message.body.present?)}
+                //     <div class="right-contents__center__bottom__message">
+                //       ${message.body}
+                //       ${image_tag message.image.url if message.image.present?}
+                //     </div>
+                //   ${end}
+                // </div>
+    return html;
+  }
+
   $('.new_message').on('submit', function(e){
 
     e.preventDefault();
@@ -16,11 +38,13 @@ $(function(){
     })
 
     .done(function(message){
-      console.log(message);
+      let html = build_message(message);
+      $('.right-contents__center').append(html);
+      $('.right-contents__bottom__message').val('');
     })
 
     .fail(function(){
-
+      alert('エラー');
     })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,1 +1,4 @@
+json.name @message.user.name
 json.body @message.body
+json.image @message.image
+json.date @message.created_at
