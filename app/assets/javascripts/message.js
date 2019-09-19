baa3933f103a9125514b4814d41976e855dc7dfc
na229aih <naoya.aihara.1127@gmail.com>
2019-09-19 16:25:47 +0900

    add jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -14,5 +14,13 @@ $(function(){
       processData: false,
       contentType: false
     })
+
+    .done(function(message){
+      console.log(message);
+    })
+
+    .fail(function(){
+
+    })
   })
 });
