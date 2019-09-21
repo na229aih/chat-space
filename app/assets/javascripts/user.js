af12444cec30c1af9f77cb5bbf983c1d0e0055f0
na229aih <naoya.aihara.1127@gmail.com>
2019-09-21 16:11:35 +0900

    add click ivent

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,12 +5,12 @@ $(function() {
   function appendUser(user) {
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     search_list.append(html);
   }
 
-  function appendError(user) {
+  function appendError() {
     let html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">一致するユーザーが見つかりません</p>
                 </div>`
@@ -38,9 +38,18 @@ $(function() {
         appendError();
       }
     })
+
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
 
   });
+
+  $(document).on("click", ".user-search-add", function(){
+    user_box = $(this).parent();
+    user_box.remove();
+    $(".chat-group-users.js-add-user").append(user_box);
+    user_box.remove(".user-search-add");
+  });
+
 });
